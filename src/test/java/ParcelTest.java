3651ff3bbf5afac2d48dea50cb473bf3ee3849bf
Pedram Valiani <pedram@Pedrams-MacBook-Pro.local>
2021-05-12 21:51:11 +0100

    can get length of a small parcel - test passes

--- src/test/java/ParcelTest.java
+++ src/test/java/ParcelTest.java
@@ -1,19 +1,61 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class ParcelTest {
 
-    Parcel parcel;
+    Parcel smallParcel;
+    Parcel mediumParcel;
+    Parcel largeParcel;
+    Parcel XLParcel;
 
     @Before
     public void before(){
-        Parcel smallParcel = new Parcel(9,9,9,"small",3);
-        Parcel mediumParcel = new Parcel(49,49,49,"medium",8);
-        Parcel largeParcel = new Parcel(99,99,99,"large",15);
-        Parcel XLParcel = new Parcel(100,100,100,"XL",25);
+        smallParcel = new Parcel(9,9,9,"small",3);
+        mediumParcel = new Parcel(49,49,49,"medium",8);
+        largeParcel = new Parcel(99,99,99,"large",15);
+        XLParcel = new Parcel(100,100,100,"XL",25);
     }
 
+    @Test
+    public void getLength() {
+        assertEquals(9,smallParcel.getLength());
+    }
 
+    @Test
+    public void canSetLength() {
+    }
 
+    @Test
+    public void hasWidth() {
+    }
 
+    @Test
+    public void canSetWidth() {
+    }
+
+    @Test
+    public void hasHeight() {
+    }
+
+    @Test
+    public void canSetHeight() {
+    }
+
+    @Test
+    public void getType() {
+    }
+
+    @Test
+    public void setType() {
+    }
+
+    @Test
+    public void getCost() {
+    }
+
+    @Test
+    public void setCost() {
+    }
 }
