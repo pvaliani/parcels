0a49b92aa4da02e3a4b4cf1ac40c950bb929b088
Pedram Valiani <pedram@Pedrams-MacBook-Pro.local>
2021-05-13 22:58:12 +0100

    adds weight limit property to parceltype enum

--- src/main/java/ParcelType.java
+++ src/main/java/ParcelType.java
@@ -1,7 +1,18 @@
 public enum ParcelType {
-    UNASSIGNED,
-    SMALL,
-    MEDIUM,
-    LARGE,
-    XL
+    UNASSIGNED(0.000),
+    SMALL(1),
+    MEDIUM(3),
+    LARGE(6),
+    XL(10);
+
+    private final double weightLimit;
+
+    ParcelType(double weightLimit){
+        this.weightLimit = weightLimit;
+    }
+
+    public double getWeightLimit(){
+        return weightLimit;
+    }
+
 }
